776fdea048f992a54fa1e54d8c4e63b822b77686
romeo <romeoquist@gmail.com>
2024-06-20 17:31:36 -0500

    setup application and database. The application runs succesfully

---
+++ src/main/java/com/romeoh20/RNGgame/RnGgameApplication.java
@@ -0,0 +1,13 @@
+package com.romeoh20.RNGgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RnGgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RnGgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romeoh20/RNGgame/RnGgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romeoh20.RNGgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RnGgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
