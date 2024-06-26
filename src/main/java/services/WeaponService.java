f2b743c7c3c13b69a68af9244b8197e6c0e22813
romeo <romeoquist@gmail.com>
2024-06-26 17:20:09 -0500

    Switched around WeaponService and WeaponServiceImpl.They work together now

---
+++ src/main/java/services/WeaponService.java
@@ -0,0 +1,8 @@
+package services;
+
+import models.Weapon;
+
+public interface WeaponService {
+
+    public Weapon generateWeapon();
+}
--- src/main/java/services/WeaponServiceImpl.java
+++ src/main/java/services/WeaponServiceImpl.java
@@ -1,5 +1,11 @@
 package services;
 
-public interface WeaponServiceImpl {
+import models.Weapon;
 
+public class WeaponServiceImpl implements WeaponService{
+
+    @Override
+    public Weapon generateWeapon() {
+        return null;
+    }
 }
