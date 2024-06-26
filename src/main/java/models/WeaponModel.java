3ee6a4088258f38398abe3c843142eee51ef8b0f
romeo <romeoquist@gmail.com>
2024-06-26 16:05:44 -0500

    Added lombok dependencies and created a model for weapons

---
+++ src/main/java/models/WeaponModel.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Builder
+@Entity
+public class WeaponModel {
+
+    @Id
+    private int id;
+    private String type;
+    private String subType;
+    private String rarity;
+    private String damage;
+    private String accuracy;
+    private String element;
+    private int statusEffectChance;
+
+}
